1cfef148fcd60a2a7dd552955dbe8348d2af7550
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-06-21 09:55:27 +0000

    * elementaly MVM support.
    git-svn-id: http://svn.ruby-lang.org/repos/ruby/branches/mvm@17503 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/mvm.h
+++ include/ruby/mvm.h
@@ -18,4 +18,18 @@ typedef struct rb_thread_struct rb_thread_t;
 VALUE *ruby_vm_verbose_ptr(rb_vm_t *);
 VALUE *ruby_vm_debug_ptr(rb_vm_t *);
 
+VALUE ruby_vm_get_argv(rb_vm_t*);
+void ruby_vm_set_argv(rb_vm_t *, int, char **);
+const char *ruby_vm_get_inplace_mode(rb_vm_t *);
+void ruby_vm_set_inplace_mode(rb_vm_t *, const char *);
+void ruby_vm_prog_init(rb_vm_t *);
+VALUE ruby_vm_process_options(rb_vm_t *, int, char **);
+VALUE ruby_vm_parse_options(rb_vm_t *, int, char **);
+
+void rb_vm_clear_trace_func(rb_vm_t *);
+void rb_vm_thread_terminate_all(rb_vm_t *);
+
+rb_vm_t *ruby_vm_new(void);
+int ruby_vm_run(rb_vm_t *, VALUE);
+
 #endif /* RUBY_MVM_H */
