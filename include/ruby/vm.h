c5d0becf2eb384fd58bd1dae9efa31baf1157ec5
nobu <nobu@b2dd03c8-39d4-4d8f-98ff-823fe69b080e>
2008-11-27 07:53:39 +0000

    * include/ruby/vm.h (ruby_vm_t): added for external interface.
    git-svn-id: http://svn.ruby-lang.org/repos/ruby/branches/mvm@20377 b2dd03c8-39d4-4d8f-98ff-823fe69b080e

--- include/ruby/vm.h
+++ include/ruby/vm.h
@@ -15,30 +15,31 @@
 
 /* VM type declaration */
 typedef struct rb_vm_struct rb_vm_t;
+typedef struct rb_vm_struct ruby_vm_t;
 
 /* core API */
-rb_vm_t *ruby_vm_new(int argc, char *argv[]);
-int ruby_vm_run(rb_vm_t *vm);
-int ruby_vm_start(rb_vm_t *vm);
-int ruby_vm_join(rb_vm_t *vm);
-int ruby_vm_destruct(rb_vm_t *vm);
+ruby_vm_t *ruby_vm_new(int argc, char *argv[]);
+int ruby_vm_run(ruby_vm_t *vm);
+int ruby_vm_start(ruby_vm_t *vm);
+int ruby_vm_join(ruby_vm_t *vm);
+int ruby_vm_destruct(ruby_vm_t *vm);
 
 /* initialize API */
-int ruby_vm_init_add_argv(rb_vm_t *vm, const char *arg);
-int ruby_vm_init_add_library(rb_vm_t *vm, const char *lib);
-int ruby_vm_init_add_library_path(rb_vm_t *vm, const char *path);
-int ruby_vm_init_add_expression(rb_vm_t *vm, const char *expr);
-int ruby_vm_init_script(rb_vm_t *vm, const char *script);
-int ruby_vm_init_verbose(rb_vm_t *vm, int verbose_p);
-int ruby_vm_init_debug(rb_vm_t *vm, int debug);
-int ruby_vm_init_add_initializer(rb_vm_t *vm, void (*initializer)(rb_vm_t *));
-int ruby_vm_init_stdin(rb_vm_t *vm, int fd);
-int ruby_vm_init_stdout(rb_vm_t *vm, int fd);
-int ruby_vm_init_stderr(rb_vm_t *vm, int fd);
+int ruby_vm_init_add_argv(ruby_vm_t *vm, const char *arg);
+int ruby_vm_init_add_library(ruby_vm_t *vm, const char *lib);
+int ruby_vm_init_add_library_path(ruby_vm_t *vm, const char *path);
+int ruby_vm_init_add_expression(ruby_vm_t *vm, const char *expr);
+int ruby_vm_init_script(ruby_vm_t *vm, const char *script);
+int ruby_vm_init_verbose(ruby_vm_t *vm, int verbose_p);
+int ruby_vm_init_debug(ruby_vm_t *vm, int debug);
+int ruby_vm_init_add_initializer(ruby_vm_t *vm, void (*initializer)(ruby_vm_t *));
+int ruby_vm_init_stdin(ruby_vm_t *vm, int fd);
+int ruby_vm_init_stdout(ruby_vm_t *vm, int fd);
+int ruby_vm_init_stderr(ruby_vm_t *vm, int fd);
 
 /* other API */
-void ruby_vm_foreach(int (*)(rb_vm_t *, void *), void *); /* returning false stops iteration */
-void *ruby_vm_specific_ptr(rb_vm_t *, int);
+void ruby_vm_foreach(int (*)(ruby_vm_t *, void *), void *); /* returning false stops iteration */
+void *ruby_vm_specific_ptr(ruby_vm_t *, int);
 
 
 /* system level initializer */
